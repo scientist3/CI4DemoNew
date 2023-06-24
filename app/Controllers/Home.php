798a911d8c92a34481fadc3a0357cc5957c9af52
Aamir Sofi <eraamirsofi@gmail.com>
2023-06-24 16:42:30 +0530

    added new variable

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -13,6 +13,7 @@ class Home extends BaseController
         Dummy data is mock data generated at random as a substitute for live data in testing environments. In other words, dummy data acts as a placeholder for live data, the latter of which testers only introduce once it’s determined that the trail program does not have any unintended, negative impact on the underlying data.
         For example, a company implementing a new accounting system uses dummy data to ensure its You can think of dummy data like a car crash dummy — you hope nothing bad happens to it while testing, but if something bad does happen, you can fix the problem with no negative impact on your valuable data assets.
         ";
+        $data['simple'] = "simple";
         return view('home', $data);
     }
 }
